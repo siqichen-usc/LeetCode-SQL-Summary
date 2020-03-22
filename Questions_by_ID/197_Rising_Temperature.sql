51f9f8b5199cfc5c700886ef00a38686b6704b42
Siqi Chen <41908319+SiqiChen-USC-MSBA@users.noreply.github.com>
2020-03-22 10:54:39 -0700

    Update 197_Rising_Temperature.sql

--- Questions_by_ID/197_Rising_Temperature.sql
+++ Questions_by_ID/197_Rising_Temperature.sql
@@ -4,3 +4,14 @@ FROM weather w1
 JOIN weather w2
     ON DATEADD(day,1,w1.recorddate) = w2.recorddate
 WHERE w1.temperature < w2.temperature
+
+
+
+-- Solution: Window Function, Subquery
+SELECT id
+FROM (
+    SELECT *, LAG(temperature) OVER (ORDER BY recorddate) AS prev_temp,
+        LAG(recorddate) OVER (ORDER BY recorddate) prev_date
+    FROM weather
+    ) tb1
+WHERE DATEADD(day,1,prev_date) = recorddate AND prev_temp < temperature
