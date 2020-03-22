544927c43713b5498d23522741a449c04369db0f
Siqi Chen <41908319+SiqiChen-USC-MSBA@users.noreply.github.com>
2020-03-22 11:24:36 -0700

    Create 262_Trips_and_Users.sql

---
+++ Join/Simple-Join/262_Trips_and_Users.sql
@@ -0,0 +1,17 @@
+SELECT t.Request_at AS 'Day', 
+    CAST(
+        AVG(CASE 
+                WHEN status = 'completed' then 0
+                ELSE 1.0
+            END)
+        AS DECIMAL(3,2)
+    ) AS 'Cancellation Rate'
+FROM Trips AS t
+INNER JOIN Users c 
+    ON t.Client_Id=c.Users_Id
+INNER JOIN Users d 
+    ON t.Driver_Id=d.Users_Id
+WHERE c.Banned='no' AND
+      d.Banned='no' AND
+      t.Request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY t.Request_at;
