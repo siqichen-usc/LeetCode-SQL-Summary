2f5498ca76b50d8f75968dfa2c924c5bcb6b85aa
Siqi Chen <41908319+SiqiChen-USC-MSBA@users.noreply.github.com>
2020-03-22 11:21:12 -0700

    Create 262_Trips_and_Users.sql

---
+++ Questions_by_ID/262_Trips_and_Users.sql
@@ -0,0 +1,38 @@
+-- Solution 1: Subquery
+WITH t1 AS (
+    SELECT request_at, 
+        CASE 
+            WHEN status = 'completed' then 0
+            ELSE 1.0
+        END AS status
+    FROM trips t
+    WHERE client_id NOT IN (SELECT users_id FROM users WHERE banned = 'Yes')
+        AND driver_id NOT IN (SELECT users_id FROM users WHERE banned = 'Yes')
+        AND request_at BETWEEN '2013-10-01' AND '2013-10-03'
+)
+
+SELECT request_at AS 'Day',
+    CAST(AVG(status) AS DECIMAL(4,2)) AS 'Cancellation Rate'
+FROM t1
+GROUP BY request_at;
+
+
+
+-- Solution 2: Join
+SELECT t.Request_at AS 'Day', 
+    CAST(
+        AVG(CASE 
+                WHEN status = 'completed' then 0
+                ELSE 1.0
+            END)
+        AS DECIMAL(3,2)
+    ) AS 'Cancellation Rate'
+FROM Trips AS t
+INNER JOIN Users c 
+    ON t.Client_Id=c.Users_Id
+INNER JOIN Users d 
+    ON t.Driver_Id=d.Users_Id
+WHERE c.Banned='no' AND
+      d.Banned='no' AND
+      t.Request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY t.Request_at;
