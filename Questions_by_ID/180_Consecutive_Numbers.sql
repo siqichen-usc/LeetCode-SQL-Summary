e03d6bf5410edfa5dea40301a21334058136a8aa
Siqi Chen <41908319+SiqiChen-USC-MSBA@users.noreply.github.com>
2020-03-21 17:40:33 -0700

    Update 180_Consecutive_Numbers.sql

--- Questions_by_ID/180_Consecutive_Numbers.sql
+++ Questions_by_ID/180_Consecutive_Numbers.sql
@@ -6,7 +6,7 @@ FROM (
         LAG(num) OVER (ORDER BY id)  AS prev
     FROM logs
     ) tb1
-WHERE num = next AND next = prev
+WHERE num = next AND next = prev;
 
 
 
@@ -16,4 +16,4 @@ FROM logs l1
 JOIN logs l2
     ON l1.id = l2.id-1 AND l1.num = l2.num
 JOIN logs l3
-    ON l1.id = l3.id-2 AND l2.num = l3.num
+    ON l1.id = l3.id-2 AND l2.num = l3.num;
