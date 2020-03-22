bbc11dbbd0a6021a0eaa6424945241f90ee842e2
Siqi Chen <41908319+SiqiChen-USC-MSBA@users.noreply.github.com>
2020-03-21 17:50:44 -0700

    Update 183_Customers_Who_Never_Order.sql

--- Questions_by_ID/183_Customers_Who_Never_Order.sql
+++ Questions_by_ID/183_Customers_Who_Never_Order.sql
@@ -1,6 +1,13 @@
--- Solution: Join
-SELECT c.name customers
+-- Solution 1: Join
+SELECT c.name AS customers
 FROM customers c
 LEFT JOIN orders o
     ON c.id = o.customerid
 WHERE o.id IS NULL;
+
+
+
+-- Solution 2: Subquery
+SELECT name AS customers
+FROM customers
+WHERE id NOT IN (SELECT customerid FROM Orders);
