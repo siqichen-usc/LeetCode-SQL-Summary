0498605ed693d4f517b30a0ad428bb066a35a7c9
Siqi Chen <41908319+SiqiChen-USC-MSBA@users.noreply.github.com>
2020-03-22 11:22:16 -0700

    Create 262_Trips_and_Users.sql

---
+++ Subquery/Advanced-Subquery/262_Trips_and_Users.sql
@@ -0,0 +1,16 @@
+WITH t1 AS (
+    SELECT request_at, 
+        CASE 
+            WHEN status = 'completed' then 0
+            ELSE 1.0
+        END AS status
+    FROM trips t
+    WHERE client_id NOT IN (SELECT users_id FROM users WHERE banned = 'Yes')
+        AND driver_id NOT IN (SELECT users_id FROM users WHERE banned = 'Yes')
+        AND request_at BETWEEN '2013-10-01' AND '2013-10-03'
+)
+
+SELECT request_at AS 'Day',
+    CAST(AVG(status) AS DECIMAL(4,2)) AS 'Cancellation Rate'
+FROM t1
+GROUP BY request_at;
