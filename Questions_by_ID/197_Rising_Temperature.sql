1beba8c74cc99a57e93717dc468b02c2d7b812b8
Siqi Chen <41908319+SiqiChen-USC-MSBA@users.noreply.github.com>
2020-03-22 11:18:38 -0700

    Update 197_Rising_Temperature.sql

--- Questions_by_ID/197_Rising_Temperature.sql
+++ Questions_by_ID/197_Rising_Temperature.sql
@@ -3,7 +3,7 @@ SELECT w2.id
 FROM weather w1
 JOIN weather w2
     ON DATEADD(day,1,w1.recorddate) = w2.recorddate
-WHERE w1.temperature < w2.temperature
+WHERE w1.temperature < w2.temperature;
 
 
 
@@ -14,4 +14,4 @@ FROM (
         LAG(recorddate) OVER (ORDER BY recorddate) prev_date
     FROM weather
     ) tb1
-WHERE DATEADD(day,1,prev_date) = recorddate AND prev_temp < temperature
+WHERE DATEADD(day,1,prev_date) = recorddate AND prev_temp < temperature;
