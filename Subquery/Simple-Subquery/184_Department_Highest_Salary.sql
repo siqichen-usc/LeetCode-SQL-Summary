2c1ff4f59c2ab7ff5e3d680a52da254e5375c971
Siqi Chen <41908319+SiqiChen-USC-MSBA@users.noreply.github.com>
2020-03-21 18:07:43 -0700

    Create 184_Department_Highest_Salary.sql

---
+++ Subquery/Simple-Subquery/184_Department_Highest_Salary.sql
@@ -0,0 +1,28 @@
+-- Solution: Join, Subquery
+WITH max_salary AS (
+    SELECT departmentid, max(salary) AS max_salary
+    FROM employee
+    GROUP BY departmentid
+)
+
+SELECT d.name department, e.name employee, e.salary
+FROM max_salary ms
+JOIN employee e
+    ON ms.departmentid = e.departmentid AND ms.max_salary = e.salary
+JOIN department d
+  ON e.departmentid = d.id;
+  
+
+
+-- Solution: Join, Window Function
+SELECT department, employee, salary
+FROM (
+    SELECT d.name department, e.name employee ,salary,
+        DENSE_RANK() OVER (
+            PARTITION BY departmentid 
+            ORDER BY salary DESC) r
+    FROM employee e
+    JOIN department d
+        ON e.departmentid = d.id
+    ) AS tb1
+WHERE r = 1;
