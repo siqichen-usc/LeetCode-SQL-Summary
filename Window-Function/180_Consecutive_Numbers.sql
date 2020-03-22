279808e45c5d132573ac9ea2df4e917696251ab3
Siqi Chen <41908319+SiqiChen-USC-MSBA@users.noreply.github.com>
2020-03-21 17:18:10 -0700

    Create 180_Consecutive_Numbers.sql

---
+++ Window-Function/180_Consecutive_Numbers.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT num AS ConsecutiveNums
+FROM (
+    SELECT num,
+        LEAD(num) OVER (ORDER BY id)  AS next,
+        LAG(num) OVER (ORDER BY id)  AS prev
+    FROM logs
+    ) tb1
+WHERE num = next AND next = prev
