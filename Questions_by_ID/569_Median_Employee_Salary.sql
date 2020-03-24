3824a26d0eabb94b6fb5bfc5cd5044b01be9ff1d
Siqi Chen <41908319+SiqiChen-USC-MSBA@users.noreply.github.com>
2020-03-23 23:23:28 -0700

    Create 569_Median_Employee_Salary.sql

---
+++ Questions_by_ID/569_Median_Employee_Salary.sql
@@ -0,0 +1,15 @@
+-- Solution: Window Function, Subquery
+WITH tb1 AS (
+    SELECT *, 
+        ROW_NUMBER() OVER (PARTITION BY company ORDER BY salary) AS r,
+        COUNT(*) OVER (PARTITION BY company) AS num
+    FROM employee
+)
+
+SELECT id, company, salary
+FROM tb1
+WHERE r BETWEEN num*1.0/2 AND num*1.0/2 + 1;
+
+
+
+-- Will solve it without using any built-in SQL functions later on.
