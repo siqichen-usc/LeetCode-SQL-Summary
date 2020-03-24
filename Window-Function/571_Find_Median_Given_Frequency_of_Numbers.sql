f2bced70528cdc44ff9143dc7c4cc561e4ad2c95
Siqi Chen <41908319+SiqiChen-USC-MSBA@users.noreply.github.com>
2020-03-23 23:43:26 -0700

    Create 571_Find_Median_Given_Frequency_of_Numbers.sql

---
+++ Window-Function/571_Find_Median_Given_Frequency_of_Numbers.sql
@@ -0,0 +1,10 @@
+WITH tb1 AS (
+    SELECT *, 
+        SUM(frequency) OVER (ORDER BY number) AS cum_num,
+        SUM(frequency) OVER () AS num
+    FROM numbers
+)
+
+SELECT AVG(number*1.0) AS median
+FROM tb1
+WHERE num / 2.0 BETWEEN cum_num - frequency AND cum_num;
