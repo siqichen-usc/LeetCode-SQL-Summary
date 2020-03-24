09189727b0b6f281a061cfe943c8597a2f4d9532
Siqi Chen <41908319+SiqiChen-USC-MSBA@users.noreply.github.com>
2020-03-23 21:48:26 -0700

    Create 534_Game_Play_Analysis_III.sql

---
+++ Questions_by_ID/534_Game_Play_Analysis_III.sql
@@ -0,0 +1,16 @@
+-- Solution 1: Window Function
+SELECT player_id, event_date, 
+    SUM(games_played) OVER (
+        PARTITION BY player_id 
+        ORDER BY event_date
+    )  AS games_played_so_far
+FROM activity;
+
+
+
+-- Solution 2: Join
+SELECT a1.player_id, a1.event_date, SUM(a2.games_played) AS games_played_so_far
+FROM activity a1
+JOIN activity a2
+ON a1.player_id = a2.player_id AND a1.event_date >= a2. event_date
+GROUP BY a1.player_id, a1.event_date
