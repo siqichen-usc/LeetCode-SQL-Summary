96a4916895d8dbf30e6fd41c335501393762d742
Siqi Chen <41908319+SiqiChen-USC-MSBA@users.noreply.github.com>
2020-03-24 22:31:54 -0700

    Create 580_Count_Student_Number_in_Departments.sql

---
+++ Questions_by_ID/580_Count_Student_Number_in_Departments.sql
@@ -0,0 +1,15 @@
+-- Solution 1: Join
+SELECT d.dept_name, COUNT(student_id) AS student_number
+FROM department d
+LEFT JOIN student s
+ON d.dept_id = s.dept_id
+GROUP BY d.dept_id, d.dept_name
+ORDER BY student_number DESC, d.dept_name;
+
+
+
+-- Solution: Subquery
+SELECT DISTINCT d.dept_name,
+    (SELECT COUNT(*) FROM student where dept_id = d.dept_id)  AS student_number
+FROM department d
+ORDER BY student_number DESC, d.dept_name;
