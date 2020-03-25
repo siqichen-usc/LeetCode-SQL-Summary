84de93e3bd73caf579717aabbd37197f05f7da91
Siqi Chen <41908319+SiqiChen-USC-MSBA@users.noreply.github.com>
2020-03-25 11:51:17 -0700

    Update 580_Count_Student_Number_in_Departments.sql

--- Questions_by_ID/580_Count_Student_Number_in_Departments.sql
+++ Questions_by_ID/580_Count_Student_Number_in_Departments.sql
@@ -8,7 +8,7 @@ ORDER BY student_number DESC, d.dept_name;
 
 
 
--- Solution: Subquery
+-- Solution 2: Subquery
 SELECT DISTINCT d.dept_name,
     (SELECT COUNT(*) FROM student where dept_id = d.dept_id)  AS student_number
 FROM department d
