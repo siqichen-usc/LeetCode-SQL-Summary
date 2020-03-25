ff7e036e2ab0887e34c5d163e9cf2a06600901a2
Siqi Chen <41908319+SiqiChen-USC-MSBA@users.noreply.github.com>
2020-03-25 15:20:02 -0700

    Create 602_Friend_Requests_II_Who_Has_the_Most_Friends.sql

---
+++ Questions_by_ID/602_Friend_Requests_II_Who_Has_the_Most_Friends.sql
@@ -0,0 +1,24 @@
+-- Solution 1: Subquery
+WITH tb1 AS (
+    SELECT requester_id AS id
+    FROM request_accepted
+    UNION ALL
+    SELECT accepter_id
+    FROM request_accepted
+)
+
+SELECT TOP 1 id, COUNT(*) AS num
+FROM tb1
+GROUP BY id
+ORDER BY COUNT(*) DESC;
+
+
+
+-- Solution 2: Subquery, PIVOT-UNPIVOT
+SELECT TOP 1 upvt.id, COUNT(*) AS num
+FROM request_accepted
+UNPIVOT (
+    id FOR ids IN (requester_id, accepter_id)
+) upvt
+GROUP BY upvt.id
+ORDER BY COUNT(*) DESC;
