a4a6737397079a269d29bba805a8475879bb4774
Siqi Chen <41908319+SiqiChen-USC-MSBA@users.noreply.github.com>
2020-03-25 15:08:08 -0700

    Update 601_Human_Traffic_of_Stadium.sql

--- Questions_by_ID/601_Human_Traffic_of_Stadium.sql
+++ Questions_by_ID/601_Human_Traffic_of_Stadium.sql
@@ -15,7 +15,7 @@ WHERE num >= 3;
 
 
 
--- Solution: Join, Subquery
+-- Solution 2: Join, Subquery
 WITH tb1 AS (
     SELECT s1.id AS id1, s2.id AS id2, s3.id AS id3
     FROM stadium s1, stadium s2, stadium s3
