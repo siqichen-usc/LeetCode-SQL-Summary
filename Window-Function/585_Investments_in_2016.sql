0fd83a41bd9c6b63914e49849aaf38496a91e8b5
Siqi Chen <41908319+SiqiChen-USC-MSBA@users.noreply.github.com>
2020-03-25 12:42:36 -0700

    Create 585_Investments_in_2016.sql

---
+++ Window-Function/585_Investments_in_2016.sql
@@ -0,0 +1,8 @@
+SELECT CAST(SUM(TIV_2016) AS DECIMAL(10,2)) AS TIV_2016
+FROM (
+    SELECT TIV_2016,
+        COUNT(*) OVER (PARTITION BY TIV_2015) AS count_2015,
+        COUNT(*) OVER (PARTITION BY LAT, LON) AS count_loc
+    FROM insurance
+) tb1
+WHERE count_2015 > 1 AND count_loc = 1;
