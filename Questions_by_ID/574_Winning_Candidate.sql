42c716b6581db0a7a360fff8cd69e842eda73cd6
Siqi Chen <41908319+SiqiChen-USC-MSBA@users.noreply.github.com>
2020-03-24 21:11:10 -0700

    Create 574_Winning_Candidate.sql

---
+++ Questions_by_ID/574_Winning_Candidate.sql
@@ -0,0 +1,22 @@
+-- Solution 1: Subquery
+SELECT Name
+FROM Candidate
+WHERE id = (
+    SELECT TOP 1 CandidateId
+    FROM Vote
+    GROUP BY CandidateId
+    ORDER BY COUNT(*) DESC
+) 
+
+
+
+-- Solution 2: Join
+SELECT TOP 1 c.Name
+FROM Candidate c
+JOIN Vote v
+ON c.id = v.CandidateId
+GROUP BY c.id, c.Name
+ORDER BY COUNT(*) DESC
+
+
+
