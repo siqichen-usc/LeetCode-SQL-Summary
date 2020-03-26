a24505285f3fb1bdd9e1e12b18b69f04d9f01d83
Siqi Chen <41908319+SiqiChen-USC-MSBA@users.noreply.github.com>
2020-03-26 11:55:00 -0700

    Create 603_Consecutive_Available_Seats.sql

---
+++ Subquery/Simple-Subquery/603_Consecutive_Available_Seats.sql
@@ -0,0 +1,44 @@
+-- Soltuion 1: Window Function, Subquery
+WITH tb1 AS (
+    SELECT seat_id, row_number() OVER (ORDER BY seat_id) AS r
+    FROM cinema
+    WHERE free = 1
+),
+tb2 AS (
+    SELECT seat_id, COUNT(*) OVER (PARTITION BY (seat_id - r)) AS num
+    FROM tb1
+)
+
+SELECT seat_id
+FROM tb2
+WHERE num >= 2;
+
+
+
+-- Soltuion 2: Window Function, Subquery
+WITH tb1 AS (
+    SELECT seat_id, free AS free1,
+        LEAD(free) OVER (ORDER BY seat_id) AS free2,
+        LAG(free) OVER (ORDER BY seat_id) AS free0
+    FROM cinema
+)
+
+SELECT seat_id
+FROM tb1
+WHERE free1 = 1 AND (free2 = 1 OR free0 = 1);
+
+
+
+-- Soltuion 3: Join, Subquery
+WITH tb1 AS (
+    SELECT c2.seat_id, c1.free AS free0, c2.free AS free1, c3.free AS free2
+    FROM cinema c1
+    RIGHT JOIN cinema c2
+    ON c1.seat_id + 1 = c2.seat_id 
+    LEFT JOIN cinema c3
+    ON c2.seat_id + 1 = c3.seat_id
+)
+
+SELECT seat_id
+FROM tb1
+WHERE free1 = 1 AND (free2 = 1 OR free0 = 1);
