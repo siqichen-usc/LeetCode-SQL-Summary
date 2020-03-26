bdd3f0913fc083a99849b0beb5df6e19afc154be
Siqi Chen <41908319+SiqiChen-USC-MSBA@users.noreply.github.com>
2020-03-26 12:13:47 -0700

    Create 608_Tree_Node.sql

---
+++ Questions_by_ID/608_Tree_Node.sql
@@ -0,0 +1,9 @@
+-- Solution: CASE WHEN
+SELECT id,
+    CASE
+        WHEN p_id IS NULL THEN 'Root'
+        WHEN id IN (SELECT p_id FROM tree) THEN 'Inner'
+        ELSE 'Leaf'
+    END AS Type
+FROM tree
+ORDER BY id;
