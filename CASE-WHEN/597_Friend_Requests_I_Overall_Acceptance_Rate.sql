d168a354a8030dbae16002a2b39e272b0fa00c3e
Siqi Chen <41908319+SiqiChen-USC-MSBA@users.noreply.github.com>
2020-03-27 17:44:26 -0700

    Update 597_Friend_Requests_I_Overall_Acceptance_Rate.sql

--- CASE-WHEN/597_Friend_Requests_I_Overall_Acceptance_Rate.sql
+++ CASE-WHEN/597_Friend_Requests_I_Overall_Acceptance_Rate.sql
@@ -33,3 +33,23 @@ SELECT
                 AS DECIMAL(3,2)
             ) 
     END AS accept_rate;
+
+
+
+-- Solution 3: Variable, Subquery, IIF
+DECLARE @accept DECIMAL(10,2);
+DECLARE @send DECIMAL(10,2);
+
+SELECT @accept = COUNT(*) 
+FROM (
+    SELECT DISTINCT requester_id, accepter_id 
+    FROM request_accepted
+) AS tb1;
+
+SELECT @send = IIF(COUNT(*) < 1, 1, COUNT(*)) 
+FROM (
+    SELECT DISTINCT sender_id, send_to_id 
+    FROM friend_request
+) AS tb2;
+
+Select CAST(@accept / @send AS DECIMAL(3,2)) AS accept_rate;
