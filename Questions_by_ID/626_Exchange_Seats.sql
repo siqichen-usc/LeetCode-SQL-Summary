87adf3b7c7a90ce834443461733cabe12f49388f
Siqi Chen <41908319+SiqiChen-USC-MSBA@users.noreply.github.com>
2020-03-28 18:18:47 -0700

    Create 626_Exchange_Seats.sql

---
+++ Questions_by_ID/626_Exchange_Seats.sql
@@ -0,0 +1,10 @@
+-- Solution: CASE WHEN, Subquery
+SELECT 
+    CASE
+        WHEN id % 2 = 1 AND id = (SELECT MAX(id) FROM seat) THEN id
+        WHEN id % 2 = 1 THEN id + 1
+        ELSE id - 1
+    END AS id,
+    student
+FROM seat
+ORDER BY 1;
