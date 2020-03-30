c2b9b1852858742d718f1b5e0f4afa7ea3338f49
Siqi Chen <41908319+SiqiChen-USC-MSBA@users.noreply.github.com>
2020-03-29 19:21:17 -0700

    Create 1084_Sales_Analysis_III.sql

---
+++ Questions_by_ID/1084_Sales_Analysis_III.sql
@@ -0,0 +1,12 @@
+-- Solution: Subquery
+SELECT product_id, product_name
+FROM Product
+WHERE product_id IN (
+    SELECT product_id
+    FROM Sales
+    WHERE sale_date BETWEEN '2019-01-01' AND '2019-03-31'
+    EXCEPT
+    SELECT product_id
+    FROM Sales
+    WHERE NOT sale_date BETWEEN '2019-01-01' AND '2019-03-31'
+);
