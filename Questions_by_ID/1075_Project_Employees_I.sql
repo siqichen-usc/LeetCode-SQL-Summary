0658a51adb99bb175a7b33e8436a138445dc0f49
Siqi Chen <41908319+SiqiChen-USC-MSBA@users.noreply.github.com>
2020-03-29 18:19:13 -0700

    Create 1075_Project_Employees_I.sql

---
+++ Questions_by_ID/1075_Project_Employees_I.sql
@@ -0,0 +1,6 @@
+-- Solution: Join
+SELECT p.project_id, CAST(AVG(e.experience_years*1.0) AS DECIMAL(10,2)) AS average_years
+FROM Project p
+LEFT JOIN Employee e
+ON p.employee_id = e.employee_id
+GROUP BY p.project_id;
