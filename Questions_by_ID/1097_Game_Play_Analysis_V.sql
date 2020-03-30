2c02b7fd3881a6fd88cf900f22647843483989f4
Siqi Chen <41908319+SiqiChen-USC-MSBA@users.noreply.github.com>
2020-03-29 19:37:05 -0700

    Create 1097_Game_Play_Analysis_V.sql

---
+++ Questions_by_ID/1097_Game_Play_Analysis_V.sql
@@ -0,0 +1,15 @@
+-- Solution: Subquery, Join
+SELECT a1.event_date AS install_dt,
+    COUNT(DISTINCT a1.player_id) AS installs,
+    CAST(
+        COUNT(DISTINCT a2.player_id)*1.0/COUNT(DISTINCT a1.player_id)
+        AS DECIMAL(3,2)
+    ) AS Day1_retention
+FROM (
+    SELECT player_id, MIN(event_date) AS event_date
+    FROM Activity
+    GROUP BY player_id
+) a1
+LEFT JOIN Activity a2
+ON a1.player_id = a2.player_id AND DATEADD(day,1,a1.event_date) = a2.event_date
+GROUP BY a1.event_date;
