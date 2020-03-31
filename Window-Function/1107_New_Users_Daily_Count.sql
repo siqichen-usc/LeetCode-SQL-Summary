6b76a3c682b2be8a793f1e18fe4f78937ca3e449
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-03-30 19:44:06 -0700

    Create 1107_New_Users_Daily_Count.sql

---
+++ Window-Function/1107_New_Users_Daily_Count.sql
@@ -0,0 +1,15 @@
+-- Using ROW_NUMBER() instead of RANK() or DENSE_RANK() 
+-- to avoid return several rows when user login more than once at the first date
+
+WITH tb1 AS (
+    SELECT *, 
+        ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY activity_date) AS r
+    FROM Traffic
+    WHERE activity = 'login'
+)
+
+SELECT activity_date AS login_date, COUNT(*) AS user_count
+FROM tb1
+WHERE r = 1
+GROUP BY activity_date
+HAVING activity_date >= DATEADD(DAY,-90, '2019-06-30');
