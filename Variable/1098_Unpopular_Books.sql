bcecac738bdb194223388dbfc9d87bc0ad1aa92c
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-03-30 20:19:48 -0700

    Create 1098_Unpopular_Books.sql

---
+++ Variable/1098_Unpopular_Books.sql
@@ -0,0 +1,15 @@
+-- You could update the variable to any date you want or GETDATE(), and then same analysis will be done for the new date 
+
+DECLARE @d date;
+SET @d = '2019-06-23';
+
+SELECT book_id, name
+FROM Books
+WHERE available_from <= DATEADD(month, -1, @d)
+    AND book_id NOT IN (
+        SELECT book_id
+        FROM Orders
+        WHERE dispatch_date > DATEADD(year, -1, @d)
+        GROUP BY book_id
+        HAVING SUM(quantity) >= 10 
+    );
