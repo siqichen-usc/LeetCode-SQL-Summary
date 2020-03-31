516f7ad598d76bb489fa7f43420a36b231285c98
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-03-30 20:13:58 -0700

    Update 1107_New_Users_Daily_Count.sql

--- Window-Function/1107_New_Users_Daily_Count.sql
+++ Window-Function/1107_New_Users_Daily_Count.sql
@@ -1,6 +1,9 @@
 -- Using ROW_NUMBER() instead of RANK() or DENSE_RANK() 
 -- to avoid return several rows when user login more than once at the first date
 
+DECLARE @d date;
+SET @d = DATEADD(DAY,-90, '2019-06-30');
+
 WITH tb1 AS (
     SELECT *, 
         ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY activity_date) AS r
@@ -12,4 +15,4 @@ SELECT activity_date AS login_date, COUNT(*) AS user_count
 FROM tb1
 WHERE r = 1
 GROUP BY activity_date
-HAVING activity_date >= DATEADD(DAY,-90, '2019-06-30');
+HAVING activity_date >= @d;
