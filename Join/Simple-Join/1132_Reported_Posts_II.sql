b736e42a0683dd71c5f68d20f57d98e513079257
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-03-31 14:34:49 -0700

    Create 1132_Reported_Posts_II.sql

---
+++ Join/Simple-Join/1132_Reported_Posts_II.sql
@@ -0,0 +1,11 @@
+WITH tb1 AS (
+    SELECT COUNT(DISTINCT r.post_id)*100.0/COUNT(DISTINCT a.post_id) AS daily_p
+    FROM Actions a
+    LEFT JOIN Removals r
+    ON a.post_id = r.post_id
+    WHERE a.action = 'report' AND a.extra = 'spam'
+    GROUP BY a.action_date
+)
+
+SELECT CAST(AVG(daily_p) AS DECIMAL(5,2)) AS average_daily_percent
+FROM tb1;
