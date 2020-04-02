3c15696180121713b0f5ec9fffc4a81da8d4ea35
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-01 19:34:59 -0700

    Update 1159_Market_Analysis_II.sql

--- CASE-WHEN/1159_Market_Analysis_II.sql
+++ CASE-WHEN/1159_Market_Analysis_II.sql
@@ -1,3 +1,4 @@
+-- Solution: Window Function, Subquery, Join, CASE WHEN
 WITH tb1 AS (
     SELECT seller_id, item_id
     FROM (
@@ -6,6 +7,29 @@ WITH tb1 AS (
         FROM Orders
         ) rank
     WHERE r = 2
+)
+
+SELECT u.user_id AS seller_id,
+    CASE
+        WHEN u.favorite_brand = i.item_brand THEN 'yes'
+        ELSE 'no'
+    END AS '2nd_item_fav_brand'
+FROM Users u
+LEFT JOIN tb1
+ON u.user_id = tb1.seller_id
+LEFT JOIN Items i
+ON tb1.item_id = i.item_id;
+
+
+
+-- Solution 2: Join, Subquery, CASE WHEN
+WITH tb1 AS (
+    SELECT o1.seller_id, o1.item_id
+    FROM Orders o1
+    JOIN orders o2
+    ON o1.seller_id = o2.seller_id AND o1.order_date >= o2.order_date
+    GROUP BY o1.seller_id, o1.order_date, o1.item_id
+    HAVING COUNT(*) = 2
 )
 
 SELECT u.user_id AS seller_id,
