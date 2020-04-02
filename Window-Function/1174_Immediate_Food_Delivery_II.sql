f8ee9a4a1901ea8f2497518b34274203ee2c533a
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-01 21:37:27 -0700

    Create 1174_Immediate_Food_Delivery_II.sql

---
+++ Window-Function/1174_Immediate_Food_Delivery_II.sql
@@ -0,0 +1,17 @@
+WITH tb1 AS (
+    SELECT order_date, customer_pref_delivery_date,
+        ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_date) AS r
+    FROM delivery
+)
+
+SELECT CAST(
+    AVG(
+        CASE
+            WHEN order_date = customer_pref_delivery_date THEN 100.0
+            ELSE 0
+        END
+       )
+    AS DECIMAL(5,2)
+) AS immediate_percentage
+FROM tb1
+WHERE r = 1;
