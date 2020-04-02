590169d14df15ed3b7c8bfb9cacadaf87172c7ce
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-02 15:43:21 -0700

    Create 1194_Tournament_Winners.sql

---
+++ CASE-WHEN/1194_Tournament_Winners.sql
@@ -0,0 +1,47 @@
+-- Solution 1: Subquery, Join, CASE WEHN, Window Function
+WITH tb1 AS (
+    SELECT player_id, group_id,
+        SUM(
+            CASE
+                WHEN player_id = first_player THEN first_score
+                ELSE second_score
+            END
+        ) AS tp
+    FROM players p
+    LEFT JOIN matches m
+    ON p.player_id = m.first_player OR p.player_id = m.second_player
+    GROUP BY player_id, group_id
+)
+
+SELECT group_id, player_id
+FROM (
+    SELECT player_id, group_id,
+        ROW_NUMBER() OVER (PARTITION BY group_id ORDER BY tp DESC, player_id) AS r
+    FROM tb1
+) tb2
+WHERE r = 1;
+
+
+
+-- Solution 2: Subquery, Join, CASE WEHN
+WITH tb1 AS (
+    SELECT player_id, group_id,
+        SUM(
+            CASE
+                WHEN player_id = first_player THEN first_score
+                ELSE second_score
+            END
+        ) AS tp
+    FROM players p
+    LEFT JOIN matches m
+    ON p.player_id = m.first_player OR p.player_id = m.second_player
+    GROUP BY player_id, group_id
+)
+
+SELECT l.group_id, l.player_id
+FROM tb1 l
+JOIN tb1 r
+ON l.group_id = r.group_id AND 
+    (l.tp < r.tp OR (l.tp = r.tp AND l.player_id >= r.player_id))
+GROUP BY l.group_id, l.player_id
+HAVING COUNT(*) = 1;
