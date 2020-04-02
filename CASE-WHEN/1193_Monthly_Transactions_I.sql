0fcfb5b8323bae69fecd53e06796df783e05bee3
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-02 15:10:13 -0700

    Create 1193_Monthly_Transactions_I.sql

---
+++ CASE-WHEN/1193_Monthly_Transactions_I.sql
@@ -0,0 +1,20 @@
+-- Solution 1: CASE WHEN
+SELECT LEFT(trans_date,7) AS month, country,
+    COUNT(id) AS trans_count,
+    SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+FROM Transactions
+GROUP BY LEFT(trans_date,7), country;
+
+
+
+-- Solution 2: CASE WHEN
+-- Using FORMAT to increase stability
+SELECT FORMAT(trans_date,'yyyy-MM') AS month, country,
+    COUNT(id) AS trans_count,
+    SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+FROM Transactions
+GROUP BY FORMAT(trans_date,'yyyy-MM'), country;
