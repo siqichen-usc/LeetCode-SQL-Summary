151f1a5b6f2bfcb12532f56e2382cdb179b545c2
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-02 16:52:26 -0700

    Create 1205_Monthly_Transactions_II.sql

---
+++ Join/Simple-Join/1205_Monthly_Transactions_II.sql
@@ -0,0 +1,27 @@
+WITH tb1 AS (
+    SELECT LEFT(trans_date,7) AS month, country,
+        COUNT(state) AS approved_count,
+        SUM(amount) AS approved_amount
+    FROM Transactions
+    WHERE state = 'approved'
+    GROUP BY LEFT(trans_date,7), country
+),
+tb2 AS (
+    SELECT LEFT(c.trans_date,7) AS month, country,
+        COUNT(c.trans_id) AS chargeback_count,
+        SUM(t.amount) AS chargeback_amount
+    FROM Chargebacks c
+    JOIN Transactions t
+    ON c.trans_id = t.id
+    GROUP BY LEFT(c.trans_date,7), country
+)
+
+SELECT COALESCE(tb1.month,tb2.month) AS month,
+     COALESCE(tb1.country,tb2.country) AS country,
+     ISNULL(tb1.approved_count,0) AS approved_count,
+     ISNULL(tb1.approved_amount,0) AS approved_amount,
+     ISNULL(tb2.chargeback_count,0) AS chargeback_count,
+     ISNULL(tb2.chargeback_amount,0) AS chargeback_amount
+FROM tb1
+FULL OUTER JOIN tb2
+ON tb1.month = tb2.month AND tb1.country = tb2.country;
