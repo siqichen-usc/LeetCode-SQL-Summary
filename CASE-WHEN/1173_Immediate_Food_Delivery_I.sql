6d8fbdfa37a64a1321ef0b18300ef18f42a95205
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-01 21:16:38 -0700

    Create 1173_Immediate_Food_Delivery_I.sql

---
+++ CASE-WHEN/1173_Immediate_Food_Delivery_I.sql
@@ -0,0 +1,10 @@
+SELECT CAST(
+    AVG(
+        CASE
+            WHEN order_date = customer_pref_delivery_date THEN 100.0
+            ELSE 0
+        END
+       )
+    AS DECIMAL(5,2)
+) AS immediate_percentage
+FROM Delivery;
