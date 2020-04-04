e3abc3741a42ddca6678132cfa8c46be85f6a49b
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-04 15:50:29 -0700

    Create 1270_All_People_Report_to_the_Given_Manager.sql

---
+++ Questions_by_ID/1270_All_People_Report_to_the_Given_Manager.sql
@@ -0,0 +1,42 @@
+-- Solution 1: Recursive CTE, Join
+WITH cte AS (
+    SELECT employee_id
+    FROM Employees
+    WHERE manager_id = 1 AND employee_id != manager_id
+    UNION ALL
+    SELECT e.employee_id
+    FROM Employees e
+    JOIN cte
+    ON e.manager_id = cte.employee_id
+)
+
+SELECT employee_id
+FROM cte
+OPTION (MAXRECURSION 3);
+
+
+-- Solution 2: Subquery
+With tb1 AS (
+    SELECT employee_id
+    FROM Employees
+    WHERE manager_id = 1 AND employee_id != manager_id
+),
+tb2 AS (
+    SELECT employee_id
+    FROM Employees
+    WHERE manager_id IN (SELECT * FROM tb1)
+),
+tb3 AS (
+    SELECT employee_id
+    FROM Employees
+    WHERE manager_id IN (SELECT * FROM tb2)
+)
+
+SELECT *
+FROM tb1
+UNION
+SELECT *
+FROM tb2
+UNION
+SELECT *
+FROM tb3;
