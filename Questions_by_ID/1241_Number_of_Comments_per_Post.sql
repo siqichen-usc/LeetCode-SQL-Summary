96c3157f823b6aec26f015dd25cb9a463964a35a
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-03 20:47:20 -0700

    Create 1241_Number_of_Comments_per_Post.sql

---
+++ Questions_by_ID/1241_Number_of_Comments_per_Post.sql
@@ -0,0 +1,7 @@
+-- Solution: Join
+SELECT s1.sub_id AS post_id, COUNT(DISTINCT s2.sub_id) AS number_of_comments
+FROM Submissions s1
+LEFT JOIN Submissions s2
+ON s1.sub_id = s2.parent_id
+WHERE s1.parent_id IS NULL
+GROUP BY s1.sub_id;
