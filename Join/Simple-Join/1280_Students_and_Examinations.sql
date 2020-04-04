b10eb5ee0f9b12209f1550f1171776f2b1a1ce09
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-04 16:31:05 -0700

    Create 1280_Students_and_Examinations.sql

---
+++ Join/Simple-Join/1280_Students_and_Examinations.sql
@@ -0,0 +1,8 @@
+SELECT s.student_id, s.student_name, b.subject_name, 
+    COUNT(e.student_id) AS attended_exams
+FROM students s
+CROSS JOIN subjects b
+LEFT JOIN examinations e
+ON s.student_id = e.student_id AND b.subject_name = e.subject_name
+GROUP BY s.student_id, s.student_name, b.subject_name
+ORDER BY s.student_id, b.subject_name;
