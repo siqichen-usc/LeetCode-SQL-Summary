00c3224baa6e9f5022fbb4a1d5824d2bc731aa2e
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-03 20:56:11 -0700

    Create 1251_Average_Selling_Price.sql

---
+++ Questions_by_ID/1251_Average_Selling_Price.sql
@@ -0,0 +1,12 @@
+-- Solution: Join
+SELECT u.product_id, 
+    CAST(
+        SUM(price*units) * 1.0 / SUM(units) 
+        AS DECIMAL(10,2)
+    ) AS average_price
+FROM UnitsSold u
+JOIN Prices p
+ON u.product_id = p.product_id 
+    AND u.purchase_date >= p.start_date 
+    AND u.purchase_date <= p.end_date
+GROUP BY u.product_id;
