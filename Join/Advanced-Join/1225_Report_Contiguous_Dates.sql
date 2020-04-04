a77593adda494c0b07f1eef756804fbde5039e72
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-03 18:55:45 -0700

    Create 1225_Report_Contiguous_Dates.sql

---
+++ Join/Advanced-Join/1225_Report_Contiguous_Dates.sql
@@ -0,0 +1,24 @@
+WITH tb1 AS (
+    SELECT f1.fail_date AS date, 'failed' AS s, COUNT(*) AS r
+    FROM Failed f1
+    JOIN Failed f2
+    ON f1.fail_date >= f2.fail_date
+    GROUP BY f1.fail_date
+    UNION
+    SELECT s1.success_date AS date, 'succeeded' AS s, COUNT(*) AS r
+    FROM Succeeded s1
+    JOIN Succeeded s2
+    ON s1.success_date >= s2.success_date
+    GROUP BY s1.success_date
+),
+tb2 AS (
+    SELECT *, 
+        DATEPART(dayofyear, date) AS r2
+    FROM tb1
+    WHERE Year(date) = 2019
+)
+
+SELECT s AS period_state, MIN(date) AS start_date, MAX(date) AS end_date
+FROM tb2
+GROUP BY r2-r, s
+ORDER BY start_date;
