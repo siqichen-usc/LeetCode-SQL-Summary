c39c9cab44bb8d91c4a605f7d4e1784b8b85b6bc
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-03 18:54:39 -0700

    Create 1225_Report_Contiguous_Dates.sql

---
+++ Subquery/Simple-Subquery/1225_Report_Contiguous_Dates.sql
@@ -0,0 +1,48 @@
+-- Solution 1: Subquery, Window Function
+ WITH tb1 AS (
+    SELECT fail_date AS date, 'failed' AS s
+    FROM Failed
+    UNION
+    SELECT success_date AS date, 'succeeded' AS s
+    FROM Succeeded
+),
+tb2 AS (
+    SELECT *, 
+        ROW_NUMBER() OVER (PARTITION BY s ORDER BY date) AS r,
+        ROW_NUMBER() OVER (ORDER BY date) AS r2
+    FROM tb1
+    WHERE Year(date) = 2019
+)
+
+SELECT s AS period_state, MIN(date) AS start_date, MAX(date) AS end_date
+FROM tb2
+GROUP BY r2-r, s
+ORDER BY start_date;
+
+
+
+-- Solution 2: Join, Subquery
+WITH tb1 AS (
+    SELECT f1.fail_date AS date, 'failed' AS s, COUNT(*) AS r
+    FROM Failed f1
+    JOIN Failed f2
+    ON f1.fail_date >= f2.fail_date
+    GROUP BY f1.fail_date
+    UNION
+    SELECT s1.success_date AS date, 'succeeded' AS s, COUNT(*) AS r
+    FROM Succeeded s1
+    JOIN Succeeded s2
+    ON s1.success_date >= s2.success_date
+    GROUP BY s1.success_date
+),
+tb2 AS (
+    SELECT *, 
+        DATEPART(dayofyear, date) AS r2
+    FROM tb1
+    WHERE Year(date) = 2019
+)
+
+SELECT s AS period_state, MIN(date) AS start_date, MAX(date) AS end_date
+FROM tb2
+GROUP BY r2-r, s
+ORDER BY start_date;
