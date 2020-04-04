d439d67f405c3c9a8a8870f9fcb6e8788ad484b3
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-04 15:19:22 -0700

    Create 1264_Page_Recommendations.sql

---
+++ CASE-WHEN/1264_Page_Recommendations.sql
@@ -0,0 +1,13 @@
+WITH f AS (
+    SELECT CASE WHEN user1_id = 1 THEN user2_id ELSE user1_id END AS fid
+    FROM Friendship
+    WHERE user1_id = 1 OR user2_id =1 
+)
+
+SELECT DISTINCT page_id AS recommended_page
+FROM Likes
+WHERE user_id IN (SELECT * FROM f)
+EXCEPT
+SELECT page_id
+FROM Likes
+WHERE user_id = 1;
