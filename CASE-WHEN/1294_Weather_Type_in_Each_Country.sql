be1983b809c2e92f9b760b45ed21134d204560f1
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-04 16:53:34 -0700

    Create 1294_Weather_Type_in_Each_Country.sql

---
+++ CASE-WHEN/1294_Weather_Type_in_Each_Country.sql
@@ -0,0 +1,14 @@
+SELECT c.country_name, w.weather_type
+FROM (
+    SELECT country_id,
+        CASE
+            WHEN AVG(weather_state*1.0) <= 15 THEN 'Cold'
+            WHEN AVG(weather_state*1.0) >= 25 THEN 'Hot'
+            ELSE 'Warm'
+        END AS weather_type
+    FROM Weather
+    WHERE LEFT(day,7) = '2019-11'
+    GROUP BY country_id
+) w
+JOIN Countries c
+ON w.country_id = c.country_id;
