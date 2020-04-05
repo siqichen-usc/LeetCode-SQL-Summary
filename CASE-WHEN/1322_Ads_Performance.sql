35ae78858e922d437d826accdeeea6ee12972ca6
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-05 16:38:10 -0700

    Update 1322_Ads_Performance.sql

--- CASE-WHEN/1322_Ads_Performance.sql
+++ CASE-WHEN/1322_Ads_Performance.sql
@@ -1,3 +1,4 @@
+-- Solution 1: CASE WHEN
 SELECT ad_id,
     CAST(
         ISNULL(
@@ -12,5 +13,25 @@ SELECT ad_id,
         AS DECIMAL(5,2)
     ) AS ctr
 FROM Ads
+GROUP BY ad_id
+ORDER BY ctr DESC, ad_id;
+
+
+
+-- Solution 2: Subquery, PIVOT/UNPIVOT, CASE WHEN
+WITH tb1 AS (
+    SELECT ad_id, [Clicked], [Viewed], [Ignored]
+    FROM Ads
+    PIVOT (
+        COUNT(action) FOR action IN ([Clicked], [Viewed], [Ignored])
+    ) pvt
+)
+
+SELECT ad_id,
+    CASE
+        WHEN SUM(Clicked+Viewed) = 0 THEN 0.0
+        ELSE CAST(SUM(Clicked)*100.0/SUM(Clicked+Viewed) AS DECIMAL(5,2))
+    END AS ctr
+FROM tb1
 GROUP BY ad_id
 ORDER BY ctr DESC, ad_id;
