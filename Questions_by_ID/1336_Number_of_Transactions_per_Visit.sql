bd04db68c3f2ff823036b11267bc79cd54507725
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-05 16:59:42 -0700

    Create 1336_Number_of_Transactions_per_Visit.sql

---
+++ Questions_by_ID/1336_Number_of_Transactions_per_Visit.sql
@@ -0,0 +1,24 @@
+-- Solution: Join, Recursive CTE, Subquery
+WITH tb1 AS (
+    SELECT v.user_id, v.visit_date, COUNT(t.transaction_date) AS c
+    FROM Visits v
+    LEFT JOIN Transactions t
+    ON v.visit_date = t.transaction_date AND v.user_id = t.user_id
+    GROUP BY v.user_id, v.visit_date
+),
+cte AS (
+    SELECT MAX(c) AS c
+    FROM tb1
+    UNION ALL
+    SELECT c-1
+    FROM cte
+    WHERE c > 0
+)
+
+SELECT cte.c AS transactions_count,
+    COUNT(tb1.user_id) AS visits_count
+FROM cte
+LEFT JOIN tb1
+ON cte.c = tb1.c
+GROUP BY cte.c
+ORDER BY transactions_count;
