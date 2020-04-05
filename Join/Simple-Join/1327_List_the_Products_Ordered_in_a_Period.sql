e24d06d21a14103af6d9ec5aa13dfa5b5fc26ea5
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-05 16:46:45 -0700

    Create 1327_List_the_Products_Ordered_in_a_Period.sql

---
+++ Join/Simple-Join/1327_List_the_Products_Ordered_in_a_Period.sql
@@ -0,0 +1,7 @@
+SELECT MIN(p.product_name) AS product_name, SUM(unit) AS unit
+FROM Orders o
+JOIN Products p
+ON o.product_id = p.product_id
+WHERE YEAR(o.order_date) = 2020 AND MONTH(o.order_date) = 2
+GROUP BY p.product_id
+HAVING SUM(unit) >= 100;
