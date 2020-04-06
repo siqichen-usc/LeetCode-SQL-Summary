1263cb2412cf00174921affc60acbeb61b475151
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-06 11:11:57 -0700

    Create 1364_Number_of_Trusted_Contacts_of_a_Customer.sql

---
+++ Questions_by_ID/1364_Number_of_Trusted_Contacts_of_a_Customer.sql
@@ -0,0 +1,22 @@
+-- Solution: Join, Subquery
+WITH tb1 AS (
+    SELECT c.customer_id, c.customer_name,
+        COUNT(con.contact_name) AS contacts_cnt,
+        COUNT(c2.customer_id) AS trusted_contacts_cnt
+    FROM Customers c
+    LEFT JOIN Contacts con
+    ON c.customer_id = con.user_id
+    LEFT JOIN Customers c2
+    ON con.contact_email = c2.email
+    GROUP BY c.customer_id, c.customer_name
+)
+
+SELECT i.invoice_id, 
+    tb1.customer_name, 
+    i.price, 
+    tb1.contacts_cnt,
+    tb1.trusted_contacts_cnt
+FROM Invoices i
+JOIN tb1
+ON i.user_id = tb1.customer_id
+ORDER BY i.invoice_id;
