53c5712980ace36fcb349c7de3ecc68db2e1420e
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-07 13:43:17 -0700

    Create 1384_Total_Sales_Amount_by_Year.sql

---
+++ Questions_by_ID/1384_Total_Sales_Amount_by_Year.sql
@@ -0,0 +1,22 @@
+-- Solution: Recursive CTE, Join
+WITH cte AS (
+    SELECT MIN(period_start) AS date, MAX(period_end) AS end_date
+    FROM Sales
+    UNION ALL
+    SELECT DATEADD(day,1,date), end_date
+    FROM cte
+    WHERE date < end_date
+)
+
+SELECT s.product_id AS PRODUCT_ID,
+    p.product_name AS PRODUCT_NAME, 
+    CAST(Year(cte.date) AS CHAR(4)) AS REPORT_YEAR, 
+    SUM(average_daily_sales) AS TOTAL_AMOUNT
+FROM cte
+JOIN Sales s
+ON cte.date BETWEEN s.period_start AND s.period_end
+JOIN Product p
+ON s.product_id = p.product_id
+GROUP BY s.product_id, p.product_name, Year(cte.date)
+ORDER BY PRODUCT_ID, REPORT_YEAR
+OPTION (MAXRECURSION 0);
