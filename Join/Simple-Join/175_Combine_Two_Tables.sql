6648ec2c7d18f3fce1797be2d4b5bb4a2f782eef
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-08 23:19:33 -0700

    Update 175_Combine_Two_Tables.sql

--- Join/Simple-Join/175_Combine_Two_Tables.sql
+++ Join/Simple-Join/175_Combine_Two_Tables.sql
@@ -1,4 +1,5 @@
 SELECT p.firstname, p.lastname, a.city, a.state
 FROM person p
+-- LEFT JOIN could keep all records in the left table, and records which match with left records in right table
 LEFT JOIN address a
   ON p.personid = a.personid;
