65c8f176d3f24ff10bf4822b1f61ccb7f78b3642
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-08 23:37:36 -0700

    Update 577_Employee_Bonus.sql

--- Questions_by_ID/577_Employee_Bonus.sql
+++ Questions_by_ID/577_Employee_Bonus.sql
@@ -3,5 +3,6 @@ SELECT e.name, b.bonus
 FROM Employee e
 LEFT JOIN Bonus b
 ON e.empId = b.empId
+-- when employee has no bonus, his/her bonus will be null after left join
 WHERE b.bonus IS NULL 
     OR b.bonus < 1000;
