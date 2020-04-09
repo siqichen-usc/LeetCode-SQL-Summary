b705e4f33241714f929983f3318e2d8fe919bc49
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-08 23:45:30 -0700

    Update 1149_Article_Views_II.sql

--- Questions_by_ID/1149_Article_Views_II.sql
+++ Questions_by_ID/1149_Article_Views_II.sql
@@ -2,5 +2,6 @@
 SELECT DISTINCT viewer_id AS id
 FROM Views
 GROUP BY viewer_id, view_date
+-- When viewer viewed the same article more than once in the same day, using DISTINCT could avoid count that article twice
 HAVING COUNT(DISTINCT article_id) > 1
 ORDER BY id;
