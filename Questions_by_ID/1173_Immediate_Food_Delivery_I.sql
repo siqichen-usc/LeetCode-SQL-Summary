83d299c4e76e5ca1cf80b78ec26f50204a84b3ee
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-09 00:21:07 -0700

    Update 1173_Immediate_Food_Delivery_I.sql

--- Questions_by_ID/1173_Immediate_Food_Delivery_I.sql
+++ Questions_by_ID/1173_Immediate_Food_Delivery_I.sql
@@ -1,9 +1,9 @@
 -- Solution: CASE WHEN
 SELECT CAST(
     AVG(
+        -- using 100.0 rather than 100 is to ensure the type of number is float
+        -- so that the number will keep digits after the decimal point when averaging
         CASE
-            -- using 100.0 rather than 100 is to ensure the type of number is float
-            -- so that the number will have digits after the decimal point when averaging
             WHEN order_date = customer_pref_delivery_date THEN 100.0
             ELSE 0
         END
