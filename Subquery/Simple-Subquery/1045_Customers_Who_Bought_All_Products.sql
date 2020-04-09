e8b0b2cdc3d45656669f3dd7fdac97ec0e0a6be9
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-08 23:57:45 -0700

    Update 1045_Customers_Who_Bought_All_Products.sql

--- Subquery/Simple-Subquery/1045_Customers_Who_Bought_All_Products.sql
+++ Subquery/Simple-Subquery/1045_Customers_Who_Bought_All_Products.sql
@@ -1,4 +1,5 @@
 SELECT customer_id
 FROM customer
 GROUP BY customer_id
+-- the number of different products bought by customer equals to number of existing products
 HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(*) FROM product);
