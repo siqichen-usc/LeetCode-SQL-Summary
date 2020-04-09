794bb813b834ade8b8891dab0d4a89f5f3987d3b
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-09 00:07:59 -0700

    Update 610_Triangle_Judgement.sql

--- Questions_by_ID/610_Triangle_Judgement.sql
+++ Questions_by_ID/610_Triangle_Judgement.sql
@@ -1,6 +1,7 @@
 -- Solution: CASE WHEN
 SELECT *,
     CASE
+        -- the sum of any 2 sides of a triangle must be greater than the measure of the third side
         WHEN x+y > z AND y+z > x AND z+x > y THEN 'Yes'
         ELSE 'No'
     END AS triangle
