b118afb7f40c3a0af937f6cd33414e034f665687
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-08 23:09:17 -0700

    Update 197_Rising_Temperature.sql

--- Subquery/Simple-Subquery/197_Rising_Temperature.sql
+++ Subquery/Simple-Subquery/197_Rising_Temperature.sql
@@ -1,6 +1,8 @@
 SELECT id
 FROM (
-    SELECT *, LAG(temperature) OVER (ORDER BY recorddate) AS prev_temp,
+    SELECT *, 
+        -- Get temperature and date of previous record date, if none get null
+        LAG(temperature) OVER (ORDER BY recorddate) AS prev_temp,
         LAG(recorddate) OVER (ORDER BY recorddate) prev_date
     FROM weather
     ) tb1
