0e2babdeb01eb044b3e6155a0f7edbf91cc9822b
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-09 00:01:34 -0700

    Update 1241_Number_of_Comments_per_Post.sql

--- Join/Simple-Join/1241_Number_of_Comments_per_Post.sql
+++ Join/Simple-Join/1241_Number_of_Comments_per_Post.sql
@@ -1,6 +1,7 @@
 SELECT s1.sub_id AS post_id, COUNT(DISTINCT s2.sub_id) AS number_of_comments
 FROM Submissions s1
 LEFT JOIN Submissions s2
+-- Orginal posts are in left table, and comments are in right table
 ON s1.sub_id = s2.parent_id
 WHERE s1.parent_id IS NULL
 GROUP BY s1.sub_id;
