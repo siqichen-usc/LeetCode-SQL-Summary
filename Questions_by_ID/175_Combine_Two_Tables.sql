1e7612fa347459b7db4262c55bcc4ca7f5780b6e
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-08 23:26:39 -0700

    Update 175_Combine_Two_Tables.sql

--- Questions_by_ID/175_Combine_Two_Tables.sql
+++ Questions_by_ID/175_Combine_Two_Tables.sql
@@ -1,6 +1,6 @@
 -- Solution: Join
 SELECT p.firstname, p.lastname, a.city, a.state
 FROM person p
--- LEFT JOIN could keep all records in the left table, and records which match with left records in right table
+-- LEFT JOIN return all records from the left table, and the matched records from the right table
 LEFT JOIN address a
   ON p.personid = a.personid
