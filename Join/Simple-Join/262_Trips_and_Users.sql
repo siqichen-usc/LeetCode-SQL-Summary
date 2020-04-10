5d976d735a0691c8a38b0a0df2921f8bc391e575
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-09 23:37:14 -0700

    Update 262_Trips_and_Users.sql

--- Join/Simple-Join/262_Trips_and_Users.sql
+++ Join/Simple-Join/262_Trips_and_Users.sql
@@ -11,6 +11,7 @@ INNER JOIN Users c
     ON t.Client_Id=c.Users_Id
 INNER JOIN Users d 
     ON t.Driver_Id=d.Users_Id
+-- filter our banned client and driver and limit time frame
 WHERE c.Banned='no' AND
       d.Banned='no' AND
       t.Request_at BETWEEN '2013-10-01' AND '2013-10-03'
