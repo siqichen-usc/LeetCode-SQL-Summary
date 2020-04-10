641b8025dc9aeaf5d22731c616a05eedac4cf4d7
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-10 00:23:52 -0700

    Update 550_Game_Play_Analysis_IV.sql

--- Window-Function/550_Game_Play_Analysis_IV.sql
+++ Window-Function/550_Game_Play_Analysis_IV.sql
@@ -1,32 +1,19 @@
--- Solution 1: Subquery, Join, Window Function
-SELECT CAST(
-    COUNT(DISTINCT a.player_id)*1.0/COUNT(DISTINCT tb1.player_id)
-    AS DECIMAL(10,2)
-    ) AS fraction
-FROM (
-    SELECT player_id, event_date, 
-        DENSE_RANK() OVER (PARTITION BY player_id ORDER BY event_date) AS r
-    FROM activity) tb1
-LEFT JOIN activity a
-ON tb1.r = 1 AND tb1.player_id = a.player_id 
-    AND dateadd(day,1, tb1.event_date) = a.event_date;
-    
- 
- 
--- Solution 2: Subquery, Window Function
 WITH tb2 as (
-SELECT player_id 
-FROM (
-    SELECT player_id, 
-        MIN(event_date) OVER (PARTITION BY player_id) AS event_date,
-        LEAD(event_date) OVER (PARTITION BY player_id ORDER BY event_date) AS lead_date
-    FROM activity
-    ) tb1
-WHERE dateadd(day,1, event_date) = lead_date
+    SELECT player_id 
+    FROM (
+        SELECT player_id, 
+            -- the first-logged-in date
+            MIN(event_date) OVER (PARTITION BY player_id) AS event_date,
+            -- the next-logged-in date after first-logged-in date
+            LEAD(event_date) OVER (PARTITION BY player_id ORDER BY event_date) AS lead_date
+        FROM activity
+        ) tb1
+    -- only keep users the next-logged-in date after first-logged-in date is the day after first-logged in date
+    WHERE dateadd(day,1, event_date) = lead_date
 )
 
 SELECT CAST(
     COUNT(DISTINCT player_id)*1.0/(SELECT COUNT(DISTINCT player_id) FROM activity)
-    AS DECIMAL(10,2)
+    AS DECIMAL(3,2)
     ) AS fraction
 FROM tb2;
