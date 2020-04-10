8b5b030f800a5ae869ed2d1900b2ce31909d7be0
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-09 23:56:52 -0700

    Update 184_Department_Highest_Salary.sql

--- Subquery/Simple-Subquery/184_Department_Highest_Salary.sql
+++ Subquery/Simple-Subquery/184_Department_Highest_Salary.sql
@@ -18,6 +18,8 @@ JOIN department d
 SELECT department, employee, salary
 FROM (
     SELECT d.name department, e.name employee ,salary,
+        -- ranking salaries in each department
+        -- the employee with highest salary will get r = 1
         DENSE_RANK() OVER (
             PARTITION BY departmentid 
             ORDER BY salary DESC) r
