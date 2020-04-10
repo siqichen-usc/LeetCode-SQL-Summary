d57e141d12ffb2fcd79c814171b09de690a09d58
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-09 23:30:58 -0700

    Update 1341_Movie_Rating.sql

--- Subquery/Simple-Subquery/1341_Movie_Rating.sql
+++ Subquery/Simple-Subquery/1341_Movie_Rating.sql
@@ -1,6 +1,7 @@
 SELECT MIN(name) AS results
 FROM Users
 WHERE user_id IN (
+    -- find users who rated greatest number of the movies
     SELECT TOP 1 WITH TIES user_id
     FROM Movie_Rating
     GROUP BY user_id
@@ -10,6 +11,7 @@ UNION ALL
 SELECT MIN(title) AS results
 FROM Movies
 WHERE movie_id IN (
+    -- find movies with highest average rating in Feb. 2020
     SELECT TOP 1 WITH TIES movie_id
     FROM Movie_Rating
     WHERE YEAR(created_at) = 2020 AND MONTH(created_at) = 2
