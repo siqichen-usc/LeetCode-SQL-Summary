7bfa8794f8f1afa6fc61521f9538a61ff7be5bd3
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-10 00:22:42 -0700

    Update 550_Game_Play_Analysis_IV.sql

--- Join/Simple-Join/550_Game_Play_Analysis_IV.sql
+++ Join/Simple-Join/550_Game_Play_Analysis_IV.sql
@@ -1,11 +1,15 @@
 SELECT CAST(
-    COUNT(DISTINCT a.player_id)*1.0/COUNT(DISTINCT tb1.player_id)
-    AS DECIMAL(10,2)
-    ) AS fraction
+    -- using DISTINCT to avoid double counting
+    COUNT(DISTINCT a2.player_id)*1.0/COUNT(DISTINCT a1.player_id)
+    AS DECIMAL(3,2)
+) AS fraction
 FROM (
-    SELECT player_id, event_date, 
-        DENSE_RANK() OVER (PARTITION BY player_id ORDER BY event_date) AS r
-    FROM activity) tb1
-LEFT JOIN activity a
-ON tb1.r = 1 AND tb1.player_id = a.player_id 
-    AND dateadd(day,1, tb1.event_date) = a.event_date;
+    -- get the first-logged-in date of each player
+    SELECT player_id, MIN(event_date) AS event_date
+    FROM Activity
+    GROUP BY player_id
+) a1
+-- if a player logged back in on the day right after the first-logged-in date, 
+-- he/she would get a matched record from table a2
+LEFT JOIN Activity a2
+ON a1.player_id = a2.player_id AND DATEADD(day,1,a1.event_date) = a2.event_date
