b682177274d824cd1483bd57d3635f665c687c95
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-09 23:52:57 -0700

    Update 1355_Activity_Participants.sql

--- Questions_by_ID/1355_Activity_Participants.sql
+++ Questions_by_ID/1355_Activity_Participants.sql
@@ -2,11 +2,13 @@
 SELECT activity
 FROM (
     SELECT activity,
+        -- Numbering activity by number of participants in ascending and descending order
         RANK() OVER (ORDER BY COUNT(*)) AS r1,
         RANK() OVER (ORDER BY COUNT(*) DESC) AS r2
     FROM Friends
     GROUP BY activity
 )  tb1
+-- fliter out activities with minimum or maximum number of participants
 WHERE r1 != 1 AND r2 != 1;
 
 
