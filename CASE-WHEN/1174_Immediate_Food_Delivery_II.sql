bb2331d1d638834211bdbdb4fa91841d27795404
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-09 23:46:19 -0700

    Update 1174_Immediate_Food_Delivery_II.sql

--- CASE-WHEN/1174_Immediate_Food_Delivery_II.sql
+++ CASE-WHEN/1174_Immediate_Food_Delivery_II.sql
@@ -1,10 +1,13 @@
 -- Solution 1: Subquery, CASE WHEN
 WITH tb1 AS (
+    -- fod refers to first order date, and fdd refers to first delivery date
     SELECT MIN(order_date) AS fod, MIN(customer_pref_delivery_date) AS fdd
     FROM Delivery
     GROUP BY customer_id
 )
 
+-- Since it is guaranteed that a customer has exactly one first order,
+-- when fod = fdd, the first order of that customer must be immediate order
 SELECT CAST(
     AVG(
         CASE
@@ -21,6 +24,7 @@ FROM tb1;
 -- Solution 2: Window Function, Subquery, CASE WHEN
 WITH tb1 AS (
     SELECT order_date, customer_pref_delivery_date,
+        -- identify first order of each customer
         ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_date) AS r
     FROM delivery
 )
