52d720e67d08af1d93447c331716dc3fecfda050
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-10 19:19:28 -0700

    Update 1393_Capital_Gain_Loss.sql

--- CASE-WHEN/1393_Capital_Gain_Loss.sql
+++ CASE-WHEN/1393_Capital_Gain_Loss.sql
@@ -1,3 +1,6 @@
+-- For this question, the last operation of a stock is always "sell" 
+-- and it is guaranteed that every "sell" has a "buy" before it,
+-- so we could simplify this to a CASE WHEN questions, otherwise, WINDOW FUNCTION could be a better choice
 SELECT stock_name,
     SUM(
         CASE 
