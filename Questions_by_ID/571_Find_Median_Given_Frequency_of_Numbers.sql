a06514af92a4c7b32a4cee2d7ecdc02bd6e5b85f
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-10 19:13:17 -0700

    Update 571_Find_Median_Given_Frequency_of_Numbers.sql

--- Questions_by_ID/571_Find_Median_Given_Frequency_of_Numbers.sql
+++ Questions_by_ID/571_Find_Median_Given_Frequency_of_Numbers.sql
@@ -1,6 +1,9 @@
 -- Solution: Window Function, Subquery
 WITH tb1 AS (
     SELECT *, 
+        -- There are cum_num numbers in TABLE numbers less than or equal to number in that record
+        -- e.g. There are cum_num = 8 numbers in TABLE numbers less than or equal to 1
+        -- so you will see [1,1,8,12] AS [Number, Frequency, cum_num, num]
         SUM(frequency) OVER (ORDER BY number) AS cum_num,
         SUM(frequency) OVER () AS num
     FROM numbers
