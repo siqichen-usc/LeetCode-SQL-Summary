0a44571a41866773ba7ee030920d4f1af6241a2f
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-12 16:20:20 -0700

    Update 618_Students_Report_By_Geography.sql

--- PIVOT-UNPIVOT/618_Students_Report_By_Geography.sql
+++ PIVOT-UNPIVOT/618_Students_Report_By_Geography.sql
@@ -1,8 +1,10 @@
 WITH tb1 AS (
+    -- Numbering student from each continent separately by their name alphabetically
     SELECT *, ROW_NUMBER() OVER (PARTITION BY continent ORDER BY name) AS r
     FROM student
 )
 
+-- each row will be a group of student name of whom from different continent and have same numbering
 SELECT [America],[Asia],[Europe]
 FROM tb1
 PIVOT (
