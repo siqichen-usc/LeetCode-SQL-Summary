39c86f8f48d807e86f5c33c7bc03d2fb113661a2
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-12 16:04:09 -0700

    Update 1164_Product_Price_at_a_Given_Date.sql

--- Window-Function/1164_Product_Price_at_a_Given_Date.sql
+++ Window-Function/1164_Product_Price_at_a_Given_Date.sql
@@ -2,6 +2,8 @@ WITH tb1 AS (
     SELECT product_id, new_price
     FROM (
         SELECT product_id, new_price,
+            -- use ROW_NUMBER() to find the last time when the price of each product was changed before '2019-08-16'
+            -- so if the price of a product had not been changed before '2019-08-16', the product will not be included in tb1
             ROW_NUMBER() OVER (PARTITION BY product_id ORDER BY change_date DESC) AS r
         FROM Products
         WHERE change_date <= '2019-08-16'
@@ -9,6 +11,8 @@ WITH tb1 AS (
     WHERE r = 1
 )
 
+-- if a product does not in tb1, which means it has no price change before '2019-08-16'
+-- assign the original price 10 to that product
 SELECT p.product_id, ISNULL(new_price,10) AS price
 FROM (
     SELECT DISTINCT product_id 
