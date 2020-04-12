6b190806b3f06df897ab41ea8259083d9906a73e
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-12 16:31:28 -0700

    Update 1126_Active_Businesses.sql

--- Questions_by_ID/1126_Active_Businesses.sql
+++ Questions_by_ID/1126_Active_Businesses.sql
@@ -1,4 +1,5 @@
 -- Solution 1: Window Function, Subquery, CASE WHEN
+-- calculate average occurences of event types amont all business
 WITH tb1 AS (
     SELECT *,
         AVG(occurences*1.0) OVER (PARTITION BY event_type) AS avg_oc
@@ -8,11 +9,14 @@ WITH tb1 AS (
 SELECT business_id
 FROM tb1
 GROUP BY business_id
+-- count number of event types of a business with occurences 
+-- greater than the average occurences of that event type among all businesses
 HAVING SUM(CASE WHEN occurences > avg_oc THEN 1 ELSE 0 END) > 1;
 
 
 
 -- Solution 2: Subquery, Join
+-- calculate average occurences of event types amont all business
 WITH tb1 AS (
     SELECT event_type, AVG(occurences*1.0) AS avg_oc
     FROM Events
@@ -22,6 +26,8 @@ WITH tb1 AS (
 SELECT business_id
 FROM Events e
 JOIN tb1
+-- use join to keep only records in which event type of a business with occurences 
+-- greater than the average occurences of that event type among all businesses
 ON e.event_type = tb1.event_type AND e.occurences > tb1.avg_oc
 GROUP BY business_id
 HAVING COUNT(*) > 1;
