3fd7cc1922cf846359b488363cb86c65d3cad782
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-12 16:43:08 -0700

    Update 1098_Unpopular_Books.sql

--- Variable/1098_Unpopular_Books.sql
+++ Variable/1098_Unpopular_Books.sql
@@ -6,6 +6,7 @@ SET @d = '2019-06-23';
 SELECT book_id, name
 FROM Books
 WHERE available_from <= DATEADD(month, -1, @d)
+    -- find books that have sold more than 10 copies in the last year
     AND book_id NOT IN (
         SELECT book_id
         FROM Orders
