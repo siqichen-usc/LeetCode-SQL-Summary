590dda585d118bc9f6054ed695cb83bc182ee6d9
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-12 16:03:01 -0700

    Update 1164_Product_Price_at_a_Given_Date.sql

--- Questions_by_ID/1164_Product_Price_at_a_Given_Date.sql
+++ Questions_by_ID/1164_Product_Price_at_a_Given_Date.sql
@@ -3,6 +3,8 @@ WITH tb1 AS (
     SELECT product_id, new_price
     FROM (
         SELECT product_id, new_price,
+            -- use ROW_NUMBER() to find the last time when the price of each product was changed before '2019-08-16'
+            -- so if the price of a product had not been changed before '2019-08-16', the product will not be included in tb1
             ROW_NUMBER() OVER (PARTITION BY product_id ORDER BY change_date DESC) AS r
         FROM Products
         WHERE change_date <= '2019-08-16'
@@ -10,6 +12,8 @@ WITH tb1 AS (
     WHERE r = 1
 )
 
+-- if a product does not in tb1, which means it has no price change before '2019-08-16'
+-- assign the original price 10 to that product
 SELECT p.product_id, ISNULL(new_price,10) AS price
 FROM (
     SELECT DISTINCT product_id 
@@ -21,12 +25,14 @@ ON p.product_id = tb1.product_id;
 
 
 -- Solution 2: Subquery, Join
+-- find the date of last time when the price of each product was changed before '2019-08-16''
 WITH tb1 AS (
     SELECT product_id, MAX(change_date) AS date
     FROM Products
     WHERE change_date <= '2019-08-16'
     GROUP BY product_id
 ),
+-- find the latest price of product which had been changed price for at least one time before '2019-08-16'
 tb2 AS (
     SELECT p.product_id, p.new_price
     FROM Products p
@@ -34,6 +40,8 @@ tb2 AS (
     ON p.product_id = tb1.product_id AND p.change_date = tb1.date
 )
 
+-- if a product does not in tb2, which means it has no price change before '2019-08-16'
+-- assign the original price 10 to that product
 SELECT p.product_id, ISNULL(new_price,10) AS price
 FROM (
     SELECT DISTINCT product_id 
