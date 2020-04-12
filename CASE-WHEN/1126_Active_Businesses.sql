a3b89df1c950e71349a2f03b5d09ce037d4dd979
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-12 16:32:55 -0700

    Update 1126_Active_Businesses.sql

--- CASE-WHEN/1126_Active_Businesses.sql
+++ CASE-WHEN/1126_Active_Businesses.sql
@@ -1,3 +1,4 @@
+-- calculate average occurences of event types amont all business
 WITH tb1 AS (
     SELECT *,
         AVG(occurences*1.0) OVER (PARTITION BY event_type) AS avg_oc
@@ -7,4 +8,6 @@ WITH tb1 AS (
 SELECT business_id
 FROM tb1
 GROUP BY business_id
+-- count number of event types of a business with occurences 
+-- greater than the average occurences of that event type among all businesses
 HAVING SUM(CASE WHEN occurences > avg_oc THEN 1 ELSE 0 END) > 1;
