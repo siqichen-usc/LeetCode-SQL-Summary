5732bd64f8ec01a5f7b335d702a36a9ad1578f14
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-12 16:19:22 -0700

    Update 618_Students_Report_By_Geography.sql

--- Subquery/Simple-Subquery/618_Students_Report_By_Geography.sql
+++ Subquery/Simple-Subquery/618_Students_Report_By_Geography.sql
@@ -1,9 +1,11 @@
 -- Solution 1: Window Function, Subquery, PIVOT/UNPIVOT
 WITH tb1 AS (
+    -- Numbering student from each continent separately by their name alphabetically
     SELECT *, ROW_NUMBER() OVER (PARTITION BY continent ORDER BY name) AS r
     FROM student
 )
 
+-- each row will be a group of student name of whom from different continent and have same numbering
 SELECT [America],[Asia],[Europe]
 FROM tb1
 PIVOT (
@@ -14,13 +16,16 @@ PIVOT (
 
 -- Solution 2: Window Function, Subquery, IFF
 WITH tb1 AS (
+    -- Numbering student from each continent separately by their name alphabetically
     SELECT ROW_NUMBER() OVER (PARTITION BY continent ORDER BY name) AS r,
         IIF(continent = 'America', name, NULL) AS America,
         IIF(continent = 'Asia', name, NULL) AS Asia,
         IIF(continent = 'Europe', name, NULL) AS Europe
     FROM student
 )
-
+                                                  
+-- Since numbering is unique in each continent, 
+-- in each numbering group (1-3 rows), there ia only one cell contained student name in each column (e.g. America, Asia, Europe) 
 SELECT MIN(America) AS America, MIN(Asia) AS Asia, MIN(Europe) AS Europe
 FROM tb1
 GROUP BY r;
