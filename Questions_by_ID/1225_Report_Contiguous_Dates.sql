40cac202ce471fabe662b946d5cc4c5645e67c71
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-13 23:41:00 -0700

    Update 1225_Report_Contiguous_Dates.sql

--- Questions_by_ID/1225_Report_Contiguous_Dates.sql
+++ Questions_by_ID/1225_Report_Contiguous_Dates.sql
@@ -14,6 +14,8 @@ tb2 AS (
     WHERE Year(date) = 2019
 )
 
+-- contiguous dates with same period state share the same r2-r,
+-- so in a group with same r2-4, the smallest date is start date and the largest date is end date
 SELECT s AS period_state, MIN(date) AS start_date, MAX(date) AS end_date
 FROM tb2
 GROUP BY r2-r, s
@@ -23,6 +25,7 @@ ORDER BY start_date;
 
 -- Solution 2: Join, Subquery
 WITH tb1 AS (
+    -- numbering date with different states in chronological order separately
     SELECT f1.fail_date AS date, 'failed' AS s, COUNT(*) AS r
     FROM Failed f1
     JOIN Failed f2
@@ -37,11 +40,14 @@ WITH tb1 AS (
 ),
 tb2 AS (
     SELECT *, 
+        -- numbering date in chronological order
         DATEPART(dayofyear, date) AS r2
     FROM tb1
     WHERE Year(date) = 2019
 )
 
+-- contiguous dates with same period state share the same r2-r,
+-- so in a group with same r2-4, the smallest date is start date and the largest date is end date
 SELECT s AS period_state, MIN(date) AS start_date, MAX(date) AS end_date
 FROM tb2
 GROUP BY r2-r, s
