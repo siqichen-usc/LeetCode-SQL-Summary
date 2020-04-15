c5e2ad4a0bb8fbd73c4a6398c1a007e9827ed6fd
Siqi Chen <41908319+siqichen-usc@users.noreply.github.com>
2020-04-15 00:33:07 -0700

    Update 579_Find_Cumulative_Salary_of_an_Employee.sql

--- Window-Function/579_Find_Cumulative_Salary_of_an_Employee.sql
+++ Window-Function/579_Find_Cumulative_Salary_of_an_Employee.sql
@@ -2,7 +2,9 @@ SELECT e.id, month,
     SUM(Salary) OVER (
         PARTITION BY id 
         ORDER BY month
+        -- get the cumulative sum of an employee's salary over a period of 3 months 
         ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) AS Salary
 FROM Employee e
+-- exclude the most recent month for each employee
 WHERE month != (SELECT MAX(Month) FROM Employee WHERE id = e.id)
 ORDER BY id, month DESC;
